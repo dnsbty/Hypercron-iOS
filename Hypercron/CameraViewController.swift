f2145662f1002f44602ae4ca4c2c83da087941cd
Dennis Beatty <dennis@dcbeatty.com>
2016-11-12 10:21:33 -0700

    live view camera that saves images to a custom photo album

---
+++ Hypercron/CameraViewController.swift
@@ -0,0 +1,135 @@
+//
+//  CameraViewController.swift
+//  Hypercron
+//
+//  Created by Dennis Beatty on 11/12/16.
+//  Copyright © 2016 Dennis Beatty. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import Photos
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, AVCapturePhotoCaptureDelegate {
+    
+    var captureSession : AVCaptureSession?
+    var videoPreviewLayer : AVCaptureVideoPreviewLayer?
+    var photoOutput : AVCapturePhotoOutput?
+    var error : NSError? = nil
+    
+    @IBOutlet weak var captureButton: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Make sure the app has permission to access the device's camera
+        if AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo) == .denied {
+            error = NSError(domain: "Camera", code: 1, userInfo: nil)
+            return
+        }
+        
+        // Get an instance of the AVCaptureDevice class to initialize a device object and provide the video as the media type parameter
+        let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        // Get an instance of the AVCaptureDeviceInput class using the previous device object
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice) as AVCaptureDeviceInput
+            
+            // Initialize the captureSession object
+            captureSession = AVCaptureSession()
+            captureSession?.sessionPreset = AVCaptureSessionPreset1920x1080
+            
+            // Set the input device on the captureSession
+            captureSession?.addInput(input as AVCaptureInput)
+            
+            // Setup the output stream
+            photoOutput = AVCapturePhotoOutput()
+            let settings = AVCapturePhotoSettings(format: [AVVideoCodecKey : AVVideoCodecJPEG])
+            photoOutput?.setPreparedPhotoSettingsArray([settings], completionHandler: nil)
+            captureSession?.addOutput(photoOutput)
+        }
+        catch let error as NSError {
+            // If any error occurs, store it for popup display and don't continue any further
+            self.error = error
+            return
+        }
+        
+        // Initialize the video preview layer and add it as a sublayer to the view preview view's layer
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        videoPreviewLayer?.frame = view.layer.bounds
+        view.layer.addSublayer(videoPreviewLayer!)
+        
+        // Start video capture
+        captureSession?.startRunning()
+        
+        // Bring UI to the front
+        view.bringSubview(toFront: captureButton)
+        
+        // Give the capture button a tap event
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(CameraViewController.capturePhoto(gestureRecognizer:)))
+        captureButton.isUserInteractionEnabled = true
+        captureButton.addGestureRecognizer(tapGestureRecognizer)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // check if there was an error while loading the view
+        if error != nil {
+            
+            // if there was see if it came from the camera
+            if error!.domain == "Camera" {
+                
+                // if it did create a popup asking the user to give permission to use the camera
+                let alert = UIAlertController(
+                    title: "Permissions Error",
+                    message: "Camera access is required in order to take photos",
+                    preferredStyle: UIAlertControllerStyle.alert
+                )
+                alert.addAction(UIAlertAction(title: "Allow Camera", style: .cancel, handler: { (alert) -> Void in
+                    UIApplication.shared.open(URL(string: UIApplicationOpenSettingsURLString)!, options: [:], completionHandler: nil)
+                }))
+                present(alert, animated: true, completion: nil)
+            } else {
+                
+                // otherwise just use a basic popup
+                let alert = UIAlertController(title: error?.localizedDescription, message: error?.localizedRecoverySuggestion, preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action: UIAlertAction!) in
+                }))
+                present(alert, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    override var prefersStatusBarHidden : Bool {
+        return true
+    }
+    
+    @IBAction func capturePhoto(gestureRecognizer: UITapGestureRecognizer) {
+        print("Capture button tapped")
+        let settings = AVCapturePhotoSettings(format: [AVVideoCodecKey : AVVideoCodecJPEG])
+        photoOutput?.capturePhoto(with: settings, delegate: self)
+    }
+    
+    func capture(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?, previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+        if photoSampleBuffer != nil {
+            let imageData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: photoSampleBuffer!, previewPhotoSampleBuffer: previewPhotoSampleBuffer)
+            let dataProvider = CGDataProvider(data: imageData as! CFData)
+            let cgImageRef = CGImage(jpegDataProviderSource: dataProvider!, decode: nil, shouldInterpolate: false, intent: CGColorRenderingIntent.relativeColorimetric)
+            
+            let image = UIImage(cgImage: cgImageRef!, scale: 1.0, orientation: .right)
+            
+            PHPhotoLibrary.saveImage(image: image, albumName: "Hypercron", completion: {
+                _ in
+                self.captureButton.image = image
+            })
+        }
+    }
+}
+
---
+++ Hypercron/PHPhotoLibraryExtension.swift
@@ -0,0 +1,122 @@
+//
+//  CustomPhotoAlbum.swift
+//  Hypercron
+//
+//  Created by Dennis Beatty on 11/12/16.
+//  Copyright © 2016 Dennis Beatty. All rights reserved.
+//
+
+import Foundation
+import Photos
+
+extension PHPhotoLibrary {
+    
+    typealias PhotoAsset = PHAsset
+    typealias PhotoAlbum = PHAssetCollection
+    
+    static func saveImage(image: UIImage, albumName: String, completion: @escaping (PHAsset?)->()) {
+        if let album = self.findAlbum(albumName: albumName) {
+            self.saveImage(image: image, album: album, completion: completion)
+            return
+        }
+        createAlbum(albumName: albumName) { album in
+            if let album = album {
+                self.saveImage(image: image, album: album, completion: completion)
+            }
+            else {
+                assert(false, "Album is nil")
+            }
+        }
+    }
+    
+    static private func saveImage(image: UIImage, album: PhotoAlbum, completion: @escaping (PHAsset?)->()) {
+        var placeholder: PHObjectPlaceholder?
+        PHPhotoLibrary.shared().performChanges({
+            // Request creating an asset from the image
+            let createAssetRequest = PHAssetChangeRequest.creationRequestForAsset(from: image)
+            // Request editing the album
+            guard let albumChangeRequest = PHAssetCollectionChangeRequest(for: album) else {
+                assert(false, "Album change request failed")
+                return
+            }
+            // Get a placeholder for the new asset and add it to the album editing request
+            guard let photoPlaceholder = createAssetRequest.placeholderForCreatedAsset else {
+                assert(false, "Placeholder is nil")
+                return
+            }
+            placeholder = photoPlaceholder
+            let fastEnumeration = NSArray(array: [photoPlaceholder] as [PHObjectPlaceholder])
+            albumChangeRequest.addAssets(fastEnumeration)
+        }, completionHandler: { success, error in
+            guard let placeholder = placeholder else {
+                assert(false, "Placeholder is nil")
+                completion(nil)
+                return
+            }
+            
+            if success {
+                completion(PHAsset.fetchAssets(withLocalIdentifiers: [placeholder.localIdentifier], options: nil).firstObject)
+            } else {
+                print(error!)
+                completion(nil)
+            }
+        })
+    }
+    
+    static func findAlbum(albumName: String) -> PhotoAlbum? {
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.predicate = NSPredicate(format: "title = %@", albumName)
+        let fetchResult = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .albumRegular, options: fetchOptions)
+        guard let photoAlbum = fetchResult.firstObject else {
+            return nil
+        }
+        return photoAlbum
+    }
+    
+    static func createAlbum(albumName: String, completion: @escaping (PhotoAlbum?)->()) {
+        var albumPlaceholder: PHObjectPlaceholder?
+        PHPhotoLibrary.shared().performChanges({
+            // Request creating an album with parameter name
+            let createAlbumRequest = PHAssetCollectionChangeRequest.creationRequestForAssetCollection(withTitle: albumName)
+            // Get a placeholder for the new album
+            albumPlaceholder = createAlbumRequest.placeholderForCreatedAssetCollection
+        }, completionHandler: { success, error in
+            guard let placeholder = albumPlaceholder else {
+                assert(false, "Album placeholder is nil")
+                completion(nil)
+                return
+            }
+            
+            
+            let fetchResult = PHAssetCollection.fetchAssetCollections(withLocalIdentifiers: [placeholder.localIdentifier], options: nil)
+            guard let album = fetchResult.firstObject else {
+                assert(false, "FetchResult has no PHAssetCollection")
+                completion(nil)
+                return
+            }
+            
+            if success {
+                completion(album)
+            }
+            else {
+                print(error!)
+                completion(nil)
+            }
+        })
+    }
+    
+    static func loadThumbnailFromLocalIdentifier(localIdentifier: String, completion: @escaping (UIImage?)->()) {
+        guard let asset = PHAsset.fetchAssets(withLocalIdentifiers: [localIdentifier], options: nil).firstObject else {
+            completion(nil)
+            return
+        }
+        loadThumbnailFromAsset(asset: asset, completion: completion)
+    }
+    
+    static func loadThumbnailFromAsset(asset: PhotoAsset, completion: @escaping (UIImage?)->()) {
+        PHImageManager.default().requestImage(for: asset, targetSize: CGSize(width: 100.0, height: 100.0), contentMode: .aspectFit, options: PHImageRequestOptions(), resultHandler: { result, info in
+            completion(result)
+        })
+    }
+    
+}
